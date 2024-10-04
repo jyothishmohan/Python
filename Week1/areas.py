4b59bf34564813db26b442c2722ce15e931d7bbb
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-10-04 11:51:08 +0530

    Add files via upload

---
+++ Week1/areas.py
@@ -0,0 +1,14 @@
+
+r=int(input("radius of circle : "))
+l=int(input("lenght of rectangle : "))
+b=int(input("breadth of rectangle : "))
+base=int(input("base of triangle : "))
+h=int(input("height of triangle : "))
+
+circle=3.14*r*r
+rectangle=l*b
+triangle=0.5*base*h
+
+print("area of circle = ", circle)
+print("area of rectangle = ", rectangle)
+print("area of triangle = ", triangle)
