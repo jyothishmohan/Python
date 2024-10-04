f5e8507d85afa433646c5aa59ad18c3e072aba2a
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-10-04 11:55:30 +0530

    Add files via upload

---
+++ Week1_27-09-2024/arithmeticop.py
@@ -0,0 +1,7 @@
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+
+print('sum = ',a+b)
+print('difference = ',a-b)
+print('division = ',a/b)
+print('multiplication = ',a*b)
