0665ff15f34958aec09dd445ad3eaac6c49ef6e9
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-10-30 10:51:16 +0530

    Add files via upload

---
+++ week5-23-10-2024/factors_of_a_number_25-10-2024.py
@@ -0,0 +1,8 @@
+num=int(input("Enter Number : "))
+list1=[]             
+for i in range(1,num+1):
+    if num%i==0:
+        list1.append(i)              
+print("factors are : ",list1)              
+              
+              
