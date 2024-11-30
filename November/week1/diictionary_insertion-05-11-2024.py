9a294779ce2bcafe8747d1b09fd59ee03b9f6442
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-11-30 19:40:22 +0530

    Update diictionary_insertion-05-11-2024.py

--- November/week1/diictionary_insertion-05-11-2024.py
+++ November/week1/diictionary_insertion-05-11-2024.py
@@ -1,28 +1,40 @@
-dict1={'Name':'Jyothish','Roll Number':25, 'Registration Number':'TMATSMT032', 'Department':'MCA', 'Semester':1}
+dict1={}
+dict1['Name']=input("Enter the name:")
+dict1['Roll number']=input("Enter the roll number:")
+dict1['registration number']=input("Enter the registration number:")
+dict1['Department']=input("Enter the Department:")
+dict1['Semester']=input("Enter the semester:")
 print("Dictionary : ",dict1)
 print()
-
-dict1['Total Mark']=int(input("Enter mark out of 100 : "))
-print("Dictionary after adding total mark : ",dict1)
 print()
 
-if dict1['Total Mark']>=90:
-    dict1['Grade']='A'
+totalmark=int(input("Enter the total mark out of 100:"))
+print("Total mark= ",totalmark)
+print()
+print()
+dict1['total mark']=totalmark
+print()
+print("Dictionary after adding total mark:",dict1)
+print()
 
-elif dict1['Total Mark']>=82:
-    dict1['Grade']='B'
-
-elif dict1['Total Mark']>=60:
-    dict1['Grade']='C'
-
-elif dict1['Total Mark']>=50:
-    dict1['Grade']='P'
-
+if dict1['total mark']>=90:
+    dict1['grade']='A'
+    
+elif dict1['total mark']>=82:
+    dict1['grade']='B'
+    
+elif dict1['total mark']>=75:
+    dict1['grade']='C'
+    
+elif dict1['total mark']>=60:
+    dict1['grade']='D'
+    
+elif dict1['total mark']>=50:
+    dict1['grade']='P'
 else:
-    dict1['Grade']='F'
-
-print("Dictionary after adding grade : ",dict1)
+    dict1['grade']='FAILED'
+print("Dictionary after adding grade:",dict1)
 print()
 
-dict1.pop('Roll Number')
-print("Dictionary after deleting roll number : ",dict1)
+dict1.pop('Roll number')
+print("Dictionary after deleting roll number:",dict1)
